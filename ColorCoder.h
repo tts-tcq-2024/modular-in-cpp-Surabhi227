b93b19905ade605949ecc5ba1a1923c872541037
Surabhi227 <surabhiravishankar@gmail.com>
2024-07-18 18:38:55 +0530

    Update ColorCoder.h

--- ColorCoder.h
+++ ColorCoder.h
@@ -1,19 +1,12 @@
-
-#pragma once
-#include <iostream>
-#include <iomanip>
+#ifndef COLORCODER_H
+#define COLORCODER_H
+ 
 #include <string>
-
+ 
 namespace TelCoColorCoder {
-    enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
-    enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
-
-    const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
-    const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-    
-    const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
-    const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+ 
     class ColorPair {
     private:
         MajorColor majorColor;
@@ -24,8 +17,10 @@ namespace TelCoColorCoder {
         MinorColor getMinor() const;
         std::string ToString() const;
     };
-
+ 
     ColorPair GetColorFromPairNumber(int pairNumber);
     int GetPairNumberFromColor(MajorColor major, MinorColor minor);
     void PrintColorCodingReference();
 }
+ 
+#endif 
